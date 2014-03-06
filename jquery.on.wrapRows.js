fdc8b87ebb3e782f4b139d2d3b28369c7adbaac7
owennicol <owennicol@gmail.com>
2014-03-06 15:13:45 +0000

    Initial commit

---
+++ jquery.on.wrapRows.js
@@ -0,0 +1,27 @@
+;
+(function ($) {
+
+
+    $.fn.wrapRows = function (options) {
+
+        var defaults = {
+            count: 4,
+            wrapperClass: 'row'
+        },
+
+        options = $.extend({}, defaults, options);
+        
+        var divs = this,
+        count = options.count,
+        wrapperClass = options.wrapperClass;
+
+        for (var i = 0; i < divs.length; i += count) {
+            divs.slice(i, i + count).wrapAll('<div class=' + wrapperClass + '></div>');
+        }
+
+    };
+
+
+
+
+})(jQuery);
